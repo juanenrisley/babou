90a74ea9854c1528172b37585953e83120698e50
Juan Enrique Alcaraz Pérez <juanenrisley@gmail.com>
2019-07-04 20:42:41 +0200

    :wrench: Add webpack configuration to `@babou/core` package

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    library: '@babou/core',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    react: 'react',
+  },
+};
